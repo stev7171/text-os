233098233b9a74c49a69a616256f0fe0558c0450
stev7171 <samluke1@icloud.com>
2023-06-21 10:50:11 -0400

    first command and made home menu

--- src/textos/OS.py
+++ src/textos/OS.py
@@ -3,9 +3,24 @@
 # This function is where you put the code that runs when your OS starts up
 def start():
     import syscalls
+    import kernel
 
     call = syscalls.System()
 
-    call.println("Hello from TextOS!")
+    call.println("==== TextOS Home Menu ====")
+    call.println("[1]: Run file")
+    call.println("[2]: List all files")
+    call.println("[3]: Edit file")
+    call.println("[4]: Exit")
 
-    call.get_input("")
+    op = call.get_input("[?]: ")
+
+    if op == "1":
+        program = input("Enter program: ")
+
+        if ".BIN" in program:
+            call.run_bin_file(program)
+        if ".RUN" in program:
+            kernel.run(program)
+        else:
+            call.println("File isn't runnable!")
